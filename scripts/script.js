7f35d46ff14833421aaf93497f01222415c9dd8e
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2022-10-28 11:45:58 +0200

    wip

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const allButtons = document.querySelector(".js-expand");
 const menuClosed = document.querySelector(".menu-button");
 const menuList = document.querySelector(".menu-content");
 
-// allButtons.forEach((	button) => {
+// allButtons.forEach((button) => {
 // 	button.addEventListener("click", function () {
 // 		console.log(button);
 // 		button.parentElement.classList.toggle("expand");
