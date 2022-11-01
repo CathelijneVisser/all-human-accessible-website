11b9fd52402421c9c59c6b6f3ffbfa02aa4529d0
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2022-11-01 12:43:25 +0100

    wip

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,12 @@ const allButtons = document.querySelector(".js-expand");
 const menuClosed = document.querySelector(".menu-button");
 const menuList = document.querySelector(".menu-content");
 
-// allButtons.forEach((button) => {
-// 	button.addEventListener("click", function () {
-// 		console.log(button);
-// 		button.parentElement.classList.toggle("expand");
-// 	});
-// });
+allButtons.forEach((button:not(".menu-button")) => {
+	button.addEventListener("click", function () {
+		console.log(button);
+		button.parentElement.classList.toggle("expand");
+	});
+});
 
 if (menuClosed) {
 	menuClosed.addEventListener("click", () => {
